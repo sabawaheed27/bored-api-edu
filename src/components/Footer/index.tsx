0148748454546be5b2c354328ee4f411921b0caf
Saba Waheed <saba_sh27@hotmail.com>
2025-05-20 17:50:09 +0200

    adde header and footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+
+const Footer = ()=>{
+    return(
+        <footer className="footer">
+            <p> Made with ❤️ using the{" "}
+        <a
+          href="https://bored-api.appbrewery.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Bored API
+        </a>
+      </p>
+        </footer>
+    )
+}
+export default Footer
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+ 
+const Header= ()=>{
+    return(
+        <header className="header">
+            <h1>Educational Activity Suggester</h1>
+        </header>
+        
+    )
+}
+
+export default Header
